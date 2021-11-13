83ebee805eb72da16592e9ab7c5b1707a1533302
Mohammad Al Jadallah <70474875+Mohammad2002-web@users.noreply.github.com>
2021-11-13 23:34:12 +0200

    Update and rename main.py to coloramaAndcowsay.py

--- coloramaAndcowsay.py
+++ coloramaAndcowsay.py
@@ -1,8 +1,13 @@
+#pip install cowsay
+
 import cowsay
 import colorama
+
+# To change the color of background and foreground color of terminal
 print(colorama.Back.BLACK)
 print(colorama.Fore.GREEN)
 
+# These shapes from the library cowsay
 cowsay.beavis('Hi')
 cowsay.daemon('Hi Daemon')
 cowsay.cow('ENG Agriculture')
