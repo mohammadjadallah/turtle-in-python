c6a14b68472176d2887cabee3160fd53cb09ff16
Mohammad Al Jadallah <70474875+Mohammad2002-web@users.noreply.github.com>
2021-11-13 23:31:08 +0200

    colorama and cowsay
    amazing libraries colorama and cowsay let's take a look at them

---
+++ main.py
@@ -0,0 +1,11 @@
+import cowsay
+import colorama
+print(colorama.Back.BLACK)
+print(colorama.Fore.GREEN)
+
+cowsay.beavis('Hi')
+cowsay.daemon('Hi Daemon')
+cowsay.cow('ENG Agriculture')
+cowsay.turtle('Turtle')
+cowsay.cheese('Cheese')
+cowsay.dragon('dragon')
