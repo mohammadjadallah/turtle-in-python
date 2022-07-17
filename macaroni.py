29da573a18e3ed74b074d9145435cd5ea373af64
Mohammad Al Jadallah <70474875+mohammadjadallah@users.noreply.github.com>
2022-07-17 19:03:49 +0300

    macaroni

---
+++ macaroni.py
@@ -0,0 +1,21 @@
+# Draw macaroni video on TikTok: developerx2
+
+from turtle import *
+
+speed(0)
+pensize(3)
+pencolor('yellow')
+Screen().bgcolor("black")
+
+# macaroni
+n = 120
+while n:
+
+    circle(n)
+    right(n)
+    n -= 1
+
+done()
+# Finished
+
+
